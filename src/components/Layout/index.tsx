5490f013592e9001b1d889a02a9b04d7bb0a12b6
konsbe <kons.be@outlook.com>
2022-02-03 19:13:09 +0200

    material ui styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+
+//using material ui to import ts styles
+import useStyles from "./css";
+
 const Layout: React.FC = ({ children }) => {
+  const classes = useStyles();
+
   return (
     <>
       <header>navabar</header>
-      <main>{children}</main>
+      <main className={classes.mainBody}>{children}</main>
       <footer>footer</footer>
     </>
   );
