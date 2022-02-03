fd9a5149c6a39fcafe424112dabdf8255a2312c2
konsbe <kons.be@outlook.com>
2022-02-04 00:23:48 +0200

    use Pagination Query

--- src/components/Opap/index.tsx
+++ src/components/Opap/index.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+//typic axios fetching data and returning a jsonfile as a response
 import axios from "axios";
 
-//typic axios fetching data and returning a jsonfile as a response
-const fetchData = () => {
-  return axios
-    .get("https://api.opap.gr/draws/v3.0/1100/visual-draw/928881")
+const fetchOpapData = async (page: number) => {
+  axios
+    .get(`https://api.opap.gr/draws/v3.0/1100/visual-draw/90000${page}`)
     .then((res) => {
       console.log(JSON.stringify(res));
       return res;
@@ -13,11 +16,54 @@ const fetchData = () => {
 };
 
 const Opap = () => {
-  //   const [kinoData, setKinoData] = useState<any>([]);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return <>asd</>;
+  const [page, setPage] = useState<number>(1);
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useQuery(["kino ", page], () => fetchOpapData(page), {
+      keepPreviousData: true,
+    });
+
+  return (
+    <>
+      <Box>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>Error: {error}</div>
+        ) : (
+          <div>Success</div>
+        )}
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ width: 300 }}
+          onClick={() => {
+            if (!isPreviousData) {
+              setPage((old) => Math.max(old - 1, 0));
+            }
+          }}
+          disabled={page === 0}
+        >
+          Previous
+        </Button>
+        <Button variant="text" disabled>
+          {page}
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ width: 300 }}
+          onClick={() => {
+            if (!isPreviousData) {
+              setPage((old) => old + 1);
+            }
+          }}
+        >
+          Next
+        </Button>
+      </Box>
+      <Box>{isFetching ? <span> Loading...</span> : null} </Box>
+    </>
+  );
 };
 
 export default Opap;
