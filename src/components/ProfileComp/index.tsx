1e5487fa106b73daddf047f11606efd046c8173d
konsbe <kons.be@outlook.com>
2022-02-04 20:42:25 +0200

    routing handling

--- src/components/ProfileComp/index.tsx
+++ src/components/ProfileComp/index.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Button } from "@mui/material";
 
-const ProfileComp = () => {
+const fetchCharacters = async (page: number) => {
+  const promise = await fetch(
+    `https://rickandmortyapi.com/api/character/${page}`
+  ).then((res) => {
+    return res.json();
+  });
+
+  return promise;
+};
+
+const ProfileComp = (): JSX.Element => {
+  const [page, setPage] = useState<number>(1);
+  const { data, isLoading, error } = useQuery(["proile", page], () =>
+    fetchCharacters(page)
+  );
+  const increasePage = () => {
+    setPage(page + 1);
+  };
+  const decrasePage = () => {
+    setPage(page - 1);
+  };
   return (
     <>
-      <p>asd</p>
+      <p>{isLoading ? "Loading.." : data.name}</p>
+      <Button color="success" onClick={increasePage}>
+        Next Character
+      </Button>
+      <Button color="error" onClick={decrasePage}>
+        Previous Character
+      </Button>
     </>
   );
 };
