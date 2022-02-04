8936907458b20547742154f400e1871142980017
konsbe <kons.be@outlook.com>
2022-02-04 14:28:41 +0200

    finish with opap plays

---
+++ src/components/ButtonComp/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+// interface Props {
+//   children: string;
+// }
+
+// const ButtonComp = ({ ...otherProps }): JSX.Element => {
+//   return <Button {...otherProps}>{...otherProps}</Button>;
+// };
+
+// export default ButtonComp;
--- src/components/Opap/index.tsx
+++ src/components/Opap/index.tsx
@@ -1,53 +1,47 @@
-import React, { useState } from "react";
-import { useQuery } from "react-query";
+import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
 //typic axios fetching data and returning a jsonfile as a response
 import axios from "axios";
 const fetchOpapData = async (page: number) => {
-  axios
-    .get(`https://api.opap.gr/draws/v3.0/1100/visual-draw/90000${page}`)
-    .then((res) => {
-      console.log(JSON.stringify(res));
-      return res;
-    })
-    .catch(() => {});
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`https://api.opap.gr/draws/v3.0/1100/visual-draw/90000${page}`)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 const Opap = () => {
   const [page, setPage] = useState<number>(1);
+  const [data, setData] = useState<any>({});
+  const [numbers, setNumbers] = useState<number>();
 
-  //useQuery from React Query  fetching, caching, synchronizing and updating server state
-  const {
-    isLoading,
-    isError,
-    error,
-    data,
-    isFetching,
-    isPreviousData,
-    isSuccess,
-  } = useQuery(["kino ", page], () => fetchOpapData(page), {
-    keepPreviousData: true,
-  });
-
+  useEffect(() => {
+    const kinoData = fetchOpapData(page);
+    console.log(kinoData);
+    kinoData.then((res: any) => {
+      setData(res);
+      console.log(res.data.winningNumbers.list);
+      setNumbers(res.data.winningNumbers.list);
+    });
+  }, []);
+  // console.log(data.data.winningNumbers.list);
+  // setNumbers(data.data.winningNumbers.list);
+  console.log(numbers);
   return (
     <>
       <Box>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : isError ? (
-          <div>Error: {error}</div>
-        ) : (
-          <div>Success</div>
-        )}
         <Button
           variant="contained"
           color="error"
           sx={{ width: 300 }}
           onClick={() => {
-            if (!isPreviousData) {
-              setPage((old) => Math.max(old - 1, 0));
-            }
+            setPage((old) => Math.max(old - 1, 0));
           }}
           disabled={page === 0}
         >
@@ -61,15 +55,14 @@ const Opap = () => {
           color="success"
           sx={{ width: 300 }}
           onClick={() => {
-            if (!isPreviousData) {
-              setPage((old) => old + 1);
-            }
+            setPage((old) => old + 1);
           }}
         >
           Next
         </Button>
+        {/* {numbers ? <span>numbers</span> : <></>} */}
+        <p>{numbers?.toString()}</p>
       </Box>
-      <Box>{isFetching ? <span> Loading...</span> : null} </Box>
     </>
   );
 };
