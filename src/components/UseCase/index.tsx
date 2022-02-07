02aa02707e8fbee06360578aa1df65cdba6e5d91
konsbe <kons.be@outlook.com>
2022-02-07 18:48:44 +0200

    SAd

--- src/components/UseCase/index.tsx
+++ src/components/UseCase/index.tsx
@@ -1,7 +1,8 @@
 import React, { FC, useEffect, useState, useMemo, useCallback } from "react";
 
 interface Props {
-  compValue: string;
+  compValue?: string;
+  value?: number;
 }
 
 const CompA: FC<Props> = ({ compValue }) => {
@@ -10,10 +11,24 @@ const CompA: FC<Props> = ({ compValue }) => {
   });
   return <div>{compValue}</div>;
 };
+//   useEffect(() => {
+
+const CompB: FC<Props> = ({ value }) => {
+  console.log("CompB render");
+  //   });
+  return <>{value}</>;
+};
+
+const CompC: FC<Props> = () => {
+  console.log("CompC render");
+  //   });
+  return <></>;
+};
 
 const UseCase = () => {
   const [count, setCount] = useState<number>(0);
-  console.log("App render");
+  const [count2, setCount2] = useState<number>(0);
+  console.log("App render", count);
 
   const [compValue, setCompValue] = useState<string>("");
 
@@ -21,16 +36,21 @@ const UseCase = () => {
     () => <CompA compValue={compValue} />,
     [compValue]
   );
-  const clickButton = useCallback(
-    () => setCompValue("hello" + Math.random()),
-    [setCompValue]
-  );
+  //   const clickButton = useCallback(
+  //     () => setCompValue("hello" + Math.random()),
+  //     [setCompValue]
+  //   );
+  const handleClick = useCallback(() => setCount(count + 1), [count]);
 
   return (
     <div>
-      <h1 onClick={() => setCount(count + 1)}>{count}</h1>
+      <h1 onClick={handleClick}>{count}</h1>
       {CompMemoized}
-      <button onClick={clickButton}>Button</button>
+      <CompB value={count} />
+      <button onClick={() => setCompValue("hello" + Math.random())}>
+        Button
+      </button>
+      <CompC value={count} />
     </div>
   );
 };
