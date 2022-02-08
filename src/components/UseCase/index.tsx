014d01629b6a6a5bcdf9ff4f73cf2edc6b404910
konsbe <kons.be@outlook.com>
2022-02-08 10:53:01 +0200

    useCallback, useMemo, React.memo

--- src/components/UseCase/index.tsx
+++ src/components/UseCase/index.tsx
@@ -1,9 +1,7 @@
 import React, { FC, useEffect, useState, useMemo, useCallback } from "react";
+import { Props } from "./interface";
 
-interface Props {
-  compValue?: string;
-  value?: number;
-}
+//Messing around with useCallback(), useMemo(), React.memo()
 
 const CompA: FC<Props> = ({ compValue }) => {
   useEffect(() => {
@@ -11,19 +9,13 @@ const CompA: FC<Props> = ({ compValue }) => {
   });
   return <div>{compValue}</div>;
 };
-//   useEffect(() => {
 
-const CompB: FC<Props> = ({ value }) => {
+// React.memo and and useCallback is a powerfull tool to memoize stuff and trigger them whenever you like
+// delete the value props from CompB to check the difference or the React.memo()
+const CompB: FC<Props> = React.memo(({ value }) => {
   console.log("CompB render");
-  //   });
   return <>{value}</>;
-};
-
-const CompC: FC<Props> = () => {
-  console.log("CompC render");
-  //   });
-  return <></>;
-};
+});
 
 const UseCase = () => {
   const [count, setCount] = useState<number>(0);
@@ -32,25 +24,25 @@ const UseCase = () => {
 
   const [compValue, setCompValue] = useState<string>("");
 
+  // useMemo something you dont have to get tricky with this and sue effece powerfull to memoize stuff
   const CompMemoized = useMemo(
     () => <CompA compValue={compValue} />,
     [compValue]
   );
-  //   const clickButton = useCallback(
-  //     () => setCompValue("hello" + Math.random()),
-  //     [setCompValue]
-  //   );
-  const handleClick = useCallback(() => setCount(count + 1), [count]);
+
+  // i want to have a callback func witch not triggers the CompB all the time
+  const handleClick = useCallback(() => setCount((c) => c + 1), [setCount]);
 
   return (
     <div>
-      <h1 onClick={handleClick}>{count}</h1>
+      <h1>useMemo(), useCallback(), React.memo()</h1>
+      <p>open the console and play with the buttons</p>
+      <button onClick={handleClick}>{count}</button>
       {CompMemoized}
       <CompB value={count} />
       <button onClick={() => setCompValue("hello" + Math.random())}>
         Button
       </button>
-      <CompC value={count} />
     </div>
   );
 };
---
+++ src/components/UseCase/interface/index.ts
@@ -0,0 +1,4 @@
+export interface Props {
+  compValue?: string;
+  value?: number;
+}
